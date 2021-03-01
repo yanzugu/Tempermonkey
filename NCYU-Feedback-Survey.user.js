7de5dccc4b0665ad9dc77358f536bc2e717af36e
yanzugu <58894500+yanzugu@users.noreply.github.com>
2021-03-02 03:50:32 +0800

    Upload files

---
+++ NCYU-Feedback-Survey.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         NCYU-Feedback-Survey
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  End boring survey quickly
+// @author       yanzugu
+// @match        https://web085004.adm.ncyu.edu.tw/Survey/Srv_StuSel.aspx
+// @grant        none
+// ==/UserScript==
+var options = {
+    '0': [null, null, null],
+    '1': ['1,5', '1,-1', '1,0'],
+    '2': ['2,4', '2,-1', '2,0'],
+    '3': ['3,3', '3,0', '3,0'],
+    '4': ['2,1', '4,-10', '4,0'],
+    '5': ['5,1', '5,-10', '5,0']
+};
+(function() {
+    'use strict';
+    var body = document.body;
+    var selects = document.getElementsByTagName('select');
+    var select = document.createElement('select');
+    select.setAttribute('style', 'margin-left:30%; height:40px; width:80px')
+    select.addEventListener("change", function() {
+    	for (let i = 0; i < selects.length; i++) {
+            for (let j = 0; j < selects[i].options.length; j++) {
+                if (selects[i].options[j].value == options[select.value][0]) {
+                    selects[i].value = options[select.value][0];
+                    break;
+                }
+                else if (selects[i].options[j].value == options[select.value][1]) {
+                    selects[i].value = options[select.value][1];
+                    break;
+                }
+                else if (selects[i].options[j].value == options[select.value][2]) {
+                    selects[i].value = options[select.value][2];
+                    break;
+                }
+            }
+        }
+    });
+    Object.keys(options).forEach(function(key) {
+        let option = document.createElement("option");
+        option.text = key;
+        option.value = key;
+        select.add(option);
+    });
+    body.appendChild(select);
+    // Your code here...
+})();
---
+++ NCYU-Timetable-Generator.user.js
@@ -0,0 +1,136 @@
+// ==UserScript==
+// @name         NCYU-Timetable-Generator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Create school timetable
+// @author       yanzugu
+// @include      /^https://web08503.\.adm\.ncyu\.edu\.tw/stu_selq02\.aspx*/
+// @grant        none
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var body = document.body;
+    var table = body.getElementsByTagName('table')[2];
+    var trs = table.getElementsByTagName('tr');
+    var classTimes = ['08:10-09:00', '09:10~10:00', '10:10~11:00', '11:10~12:00', '12:10~13:00', '13:20~14:10', '14:20~15:10', '15:20~16:10', '16:20~17:10'];
+    var days = ['一', '二', '三', '四', '五', '六', '日'];
+    var day2Num = {
+        '一': 0,
+        '二': 1,
+        '三': 2,
+        '四': 3,
+        '五': 4,
+        '六': 5,
+        '日': 6
+    };
+    var period2Num = {
+        '1': 0,
+        '2': 1,
+        '3': 2,
+        '4': 3,
+        'F': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8
+    };
+    var classes = new Array(9);
+    for (let i = 0; i < classes.length; i++) {
+        classes[i] = new Array(7);
+    }
+
+    for (let i = 1; i < trs.length; i++) {
+        let tds = trs[i].getElementsByTagName('td');
+        let className = tds[3].innerHTML;
+        let day = tds[13].innerHTML;
+        let period = tds[14].innerHTML;
+        let classRoom = tds[16].innerHTML;
+
+        for (let t = 0; t < Math.trunc(day.length / 2); t++) {
+            for (let p = period2Num[period[t * 4 + 0]]; p <= period2Num[period[t * 4 + 2]]; p++) {
+                classes[p][day2Num[day[t * 2]]] = className + '\n\n' + classRoom;
+            }
+        }
+    }
+
+    table = document.createElement('table');
+    table.setAttribute('style', "border-collapse:collapse; margin:auto; width: 100%");
+    table.setAttribute('dir', 'rtl');
+    table.classList.add('mytable');
+
+    var el = '<thead><tr class="mytr"><th class="myth"></th>';
+    for (let i = 0; i < days.length; i++) {
+        el += '<th class="myth">';
+        el += '星期' + days[i];
+        el += '</th>';
+    }
+    el += '</tr></thead>';
+
+    el += '<tbody>';
+
+    for (let i = 0; i < 9; i++) {
+        el += '<tr class="mytr">';
+        for (let j = 0; j < 8; j++) {
+            el += '<td class="mytd">';
+            if (j === 0) {
+                el += '第';
+                if (i === 4) {
+                    el += 'F';
+                } else if (i < 4) {
+                    el += (i + 1).toString();
+                } else {
+                    el += i.toString();
+                }
+                el += '節\n\n';
+                el += classTimes[i];
+            } else {
+                if (classes[i][j - 1] !== undefined) {
+                    el += classes[i][j - 1];
+                }
+            }
+            el += '</td>'
+        }
+        el += '</tr>';
+    }
+    table.innerHTML = el;
+
+    var div = document.createElement('div');
+    div.setAttribute('style', 'width:50%; margin: auto');
+    div.setAttribute('id', 'tab');
+    div.appendChild(table);
+
+    var button = document.createElement('BUTTON');
+    button.innerHTML = "列印課表";
+    button.setAttribute('style', 'magrin: auto; margin-top: 10px');
+    button.addEventListener('click', function () {
+        var sTable = document.getElementById('tab').innerHTML;
+
+        // CREATE A WINDOW OBJECT.
+        var win = window.open('', '', 'height=1000,width=1000');
+        var title = '<title>課表_' + Date.now() + '</title>';
+
+        win.document.write('<html><head>');
+        win.document.write(title); // <title> FOR PDF HEADER.
+        win.document.write('</head>');
+        win.document.write('<body>');
+        win.document.write(sTable);
+        win.document.write('</body></html>');
+
+        win.document.close(); // CLOSE THE CURRENT WINDOW.
+
+        win.print(); // PRINT THE CONTENTS.
+    });
+
+    body.appendChild(div);
+    body.appendChild(button);
+
+
+    var style = 'border: 2px solid #000;width: 12.5%;font-size: 13px;text-align: center; white-space: pre-line;';
+    $('.mytr').attr('style', style);
+    $('.myth').attr('style', style + 'height: 50px;');
+    $('.mytd').attr('style', style + 'height: 80px;');
+
+    // Your code here...
+})();
